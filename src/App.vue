ab004876ede671d1392beb9d338ce0fc4417c395
GET Academy <terje@kolderup.net>
2019-04-24 11:13:26 +0200

    x

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view :personprop="person" @datachanged="handleDatachanged"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: function() {
+    return {
+      person: {
+        name: "Terje",
+        birthYear: 1975
+      }
+    };
+  },
+  methods: {
+    handleDatachanged: function(personData) {
+      this.person.name = personData.name;
+      this.person.birthYear = personData.birthYear;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
